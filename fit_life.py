a982a145049346b57252373804f5c095bce76079
Владимир Халин <vovahalin@gmail.com>
2026-09-16 16:52:02 +0500

    3

--- fit_life.py
+++ fit_life.py
@@ -7,7 +7,7 @@ ML_IN_LITER = 1000
 print("Добро пожаловать в Fit Life!")
 print()
 
-# Ввод данных
+
 user_name = input("Привет! Введи своё имя: ").title()
 while True:
     try:
@@ -15,18 +15,29 @@ while True:
         if user_age <= 0:
             print("Возраст должен быть больше 0")
             continue
+        break
+    except ValueError:
+        print("Можно вводить только цифры!")
 
+
+while True:
+    try:
         user_weight = float(input("Введи свой вес (В кг): ").replace(',', '.'))
         if user_weight <= 0:
             print("Вес должен быть больше 0")
             continue
+        break
+    except ValueError:
+        print("Можно вводить только цифры!")
 
-        user_height = input("Введи свой рост(В метрах): ").replace(',', '.')
-        user_height = float(user_height)
+
+while True:
+    try:
+        user_height = float(input("Введи свой рост(В метрах): ")
+                            .replace(',', '.'))
         if user_height <= 0:
             print("Рост должен быть больше 0")
             continue
-
         break
     except ValueError:
         print("Можно вводить только цифры!")
@@ -34,9 +45,9 @@ while True:
 bmi = round(user_weight / (user_height ** 2), 1)
 
 # Расчёт нормы воды
-water_ml = user_weight * WATER_PER_KG
-water_l = round(water_ml / ML_IN_LITER, 2)
-# Вывод данных
+water_l = round((user_weight * WATER_PER_KG) / ML_IN_LITER, 2)
+
+
 print(f"""Отчет для пользователя: {user_name} ({user_age} г).
 Твой Индекс Массы Тела: {bmi}
 Рекомендуемая норма воды {water_l} л. в день
